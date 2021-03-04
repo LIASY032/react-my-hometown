64456777b781ff6171836f0627ad78a001825f34
LIASY032 <liasy032@mymail.unisa.edu.au>
2021-03-04 22:56:08 +1030

    （〃｀ 3′〃) add a video

--- src/components/Middle.js
+++ src/components/Middle.js
@@ -4,7 +4,7 @@ import "./Middle.css";
 function Middle() {
     return (
         <div className="middle-container">
-            
+            <video src='videos/恩平.mp4' autoPlay loop muted></video>
         </div>
     )
 }
